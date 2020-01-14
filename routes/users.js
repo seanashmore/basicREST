dcef063c46d5ccd45ed79d34f8b2b5a88a08ca51
Sean Ashmore <seanashmore@outlook.com>
2020-01-14 21:38:39 +0000

    Adding server-side validation to user signup and signin

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const {check, validationResult} = require('express-validator')
 
-router.post('/signup', async (req, res) => {
+router.post('/signup', userDetailsValidation(), async (req, res) => {
+    const isError = checkForErrors(req, res);
+    if(isError) return;
+
     const user = new User({
         email: req.body.email,
         password: req.body.password
@@ -17,8 +21,11 @@ router.post('/signup', async (req, res) => {
     }
 });
 
-router.post('/signin', async (req, res) => {
+router.post('/signin', userEmailValidation(), async (req, res) => {
     const user = User.findOne({'email': req.body.email}, (err, user) => {
+        const isError = checkForErrors(req, res);
+        if(isError) return;
+
         if(err) { res.status(404).json({ message: err}) }
 
         bcrypt.compare(req.body.password, user.password, function(error, isMatch) {
@@ -34,5 +41,27 @@ router.post('/signin', async (req, res) => {
         })
     })
 })
+
+function checkForErrors(req, res) {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        res.status(422).json({ errors: errors.array() })
+        return true
+    }
+    return false
+};
+
+function userDetailsValidation() {
+    return [
+        check('email').isEmail(),
+        check('password').isLength(6)
+    ]
+};
+
+function userEmailValidation() {
+    return [
+        check('email').isEmail()
+    ]
+}
 
 module.exports = router
